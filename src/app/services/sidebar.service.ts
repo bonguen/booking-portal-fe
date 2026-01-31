7ff0235bf769eb155b5cb8b25d6e0b6d22333b42
Burak Onguen <bonguen@deloitte.de>
2026-01-31 11:42:04 +0100

    feature-007 - create sidebarService to track toogles to show/hide hamburger menu if sidebar is active or not

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SidebarService {
+  private sidebarActive = new BehaviorSubject<boolean>(false);
+  sidebarActive$ = this.sidebarActive.asObservable();
+
+  toggleSidebar() {
+    this.sidebarActive.next(!this.sidebarActive.value);
+  }
+
+  setSidebarState(state: boolean) {
+    this.sidebarActive.next(state);
+  }
+}
